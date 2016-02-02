31ee68ca01a1cb161ae423d5639a3d4de0d04249
Dzmuh <dzmuh@outlook.com>
2016-02-03 01:51:57 +0300

    Работа с SVG тегами. Инициализация проекта. Сперва делаю в SVG, потом тоже выполняю с помощью SVG.js

---
+++ svg-tags/circle.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="typings/svgjs/svgjs.d.ts" />
+
+function draw_circle() {
+    var draw = SVG("drawing").size(50, 50).style("background:gray;");
+
+    var circle = draw.circle(20)
+        .fill("blue")
+        .cy(10)
+        .cx(25).stroke({ color: 'white', width: 4 });
+}
+
+draw_circle();
